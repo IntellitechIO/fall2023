20eadb8ae5a4e51c3a729c578d0c05b3bac82ba2
arslann05 <87247229+arslann05@users.noreply.github.com>
2023-11-19 15:05:51 -0500

    Update add_user.sh

--- ssh-stuffs/add_user.sh
+++ ssh-stuffs/add_user.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#
-sudo useradd arslan
-sudo mkdir /home/arslan/.ssh
-sudo touch /home/arslan/.ssh/authorized_keys
-sudo chmod 777 -R /home/arslan/
-cat arslan-key.pub >> /home/arslan/.ssh/authorized_keys
+
+cat arslan-key.pub >> /home/ec2-user/.ssh/authorized_keys
 
